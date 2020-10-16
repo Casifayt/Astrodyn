7ddf480ace100f72bb7b7d278ebc4062188fdaaf
CasiFayt <fayt.casimir@gmail.com>
2020-10-16 10:31:21 +0200

    Create main.m
    Initialisation of main.m code

---
+++ main.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% AERO0024 - ASTRODYNAMICS %%%
+%%%%%% ULiège - 2020-2021 %%%%%%
+
+%%%%%% Orbital propagator %%%%%%
+
+%%% Authors
+% Axel DECHAMPS - 
+% Casimir  FAYT - S196244
+
+% This project consists in the development of an orbital propagator of
+% increasing complexity. The central celestial body is chosen to be the
+% Earth.
+
+
+%% Two-body propagator %%
+
+
+%% J2-term(Earth's oblateness) %%
+
+
+%% Earth's atmosphere %%
+
+
+%% Comparison with actual satellite data %%
